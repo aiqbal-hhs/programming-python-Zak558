39798c4a458e087a3cb7d511659dc32a8987e32a
Zak558 <80420284+Zak558@users.noreply.github.com>
2021-05-11 11:45:38 +1200

    Add files via upload

---
+++ format-function-may10.py mine.py
@@ -0,0 +1,8 @@
+name = input("What is your name?")
+age = input("How old are you?")
+date of birth = input ("What is your date of birth")
+place of birth = input ("What is your place of birth")
+nationality = input ("What is your nationality")
+favorite colour = input ("What is your favorite colour")
+
+print("{} is a nice age to be, {}!".format(age, name))
