32f77cb704651a0129570ef82fac5bbb9f8295aa
Zak558 <80420284+Zak558@users.noreply.github.com>
2021-05-31 10:54:57 +1200

    Add files via upload

---
+++ comments code.py
@@ -0,0 +1,12 @@
+#Ask the user for personal details
+name = input("What is your name?")
+age = input("How old are you?")
+location = input("Where are you from?")
+ethnicity = input ("What is your ethiticity")
+
+#Print summary of input for checking
+print("This is the data you have entered: ")
+print("Name: " + name)
+print("Age: " + age)
+print("Location: " + location)
+print("Ethinticy: " + ethnicity)
